6fa1e192e2b7bbf0bbcb65eed3cbc312ed046968
Yanghwa <kellerj87@hotmail.com>
2017-03-08 14:54:40 -0500

    UI till SelectForm

--- DollarComputers/SelectForm.cs
+++ DollarComputers/SelectForm.cs
@@ -16,5 +16,24 @@ namespace DollarComputers
         {
             InitializeComponent();
         }
+
+        private void SelectForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dollarComputersDataSet.products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.dollarComputersDataSet.products);
+
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            ProductInfoForm productInfo = new ProductInfoForm();
+            this.Hide();
+            productInfo.Show();
+        }
     }
 }
